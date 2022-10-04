bbaebc16cbc723b133c7ff783c4f63e9be807648
gamal.nafel <gamal.nafel@4gtss.com>
2022-10-04 16:02:45 +0200

    add

--- mvn.sh
+++ mvn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker images
-docker run --rm -v /home/gamal/jenkinsfile/jenk/java-app:/app -v /root/.m2/:/root/.m2/ -w /app   maven:3-alpine  mvn -B -DskipTests clean package
+pwd
+docker run --rm -v /home/gamal/jenkinsfile/jenk/java-app:/app -v /root/.m2/:/root/.m2/ -w /app   maven:3-alpine pwd  mvn -B -DskipTests clean package
